420a12845bf947fed7946e7cdc1b78c3e7451cf7
yet-another-luke <lucas.schwarz@outlook.com>
2021-12-30 13:18:20 -0800

    creep and player have different hit effects. hud for health and kills.

--- Mob.gd
+++ Mob.gd
@@ -2,6 +2,7 @@ extends RigidBody2D
 
 const HitEffect = preload("res://HitEffect.tscn")
 const PlayerHurtSound = preload("res://PlayerHurtSound.tscn")
+const VanquishEffect = preload("res://VanquishEffect.tscn")
 
 signal vanquished
 
@@ -19,9 +20,9 @@ func _on_Hitbox_area_entered(area):
 
 func _on_Hurtbox_area_entered(area):
 	# print("I guess I'll just die then.")
-	var effect = HitEffect.instance()
+	var effect = VanquishEffect.instance()
 	get_tree().current_scene.add_child(effect)
 	effect.global_position = global_position - Vector2(0, 8)
-	get_tree().current_scene.add_child(PlayerHurtSound.instance())
+	# get_tree().current_scene.add_child(VanquishEffect.instance())
 	emit_signal("vanquished")
 	queue_free()
